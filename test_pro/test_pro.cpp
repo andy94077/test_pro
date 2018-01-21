c29ad0c5201b6e328dfbd2ac1ac99d58e9c1a1c2
andy94077 <andy94077@gmail.com>
2018-01-21 21:44:01 +0800

    change diff path to "diff " for all system, and format file

--- test_pro/test_pro.cpp
+++ test_pro/test_pro.cpp
@@ -4,12 +4,13 @@
 #include <sstream>
 #include <limits>
 using namespace std;
+const string diff_path = "diff ";
 #ifdef _WIN32
 #define SYSTEMPAUSE system("pause")
-	const string diff_path = "bin\\diff ", os_delim="\\", shell_script="out.bat";
+const string os_delim = "\\", shell_script = "out.bat";
 #else
 #define SYSTEMPAUSE system("bash -c \"read -n1 -rsp $\'Press any key to continue...\n\'\"")
-	const string diff_path = "diff ",os_delim="/",shell_script="out.sh";
+const string os_delim = "/", shell_script = "out.sh";
 #endif // __WIN32
 
 //#define DEBUG
@@ -20,7 +21,7 @@ inline int system(const string& str)
 }
 inline void rm_space_quotation(string& str)
 {
-	str.erase(0,str.find_first_not_of(" \t\""));
+	str.erase(0, str.find_first_not_of(" \t\""));
 	str.erase(str.find_last_not_of(" \t\"") + 1);
 }
 
@@ -28,12 +29,12 @@ void read_default(string& exe_path, string& file_dir, string& filename_extension
 {
 	string dat_path = "default.dat";
 	ifstream dat(dat_path);
-	while(dat.fail())
+	while (dat.fail())
 	{
 		cerr << "can not find\"default.dat\"" << endl;
 		cout << "please enter the full path of \"default.dat\":" << endl;
 		getline(cin, dat_path);
-		
+
 		//remove the space and quote
 		rm_space_quotation(dat_path);
 
@@ -46,25 +47,25 @@ void read_default(string& exe_path, string& file_dir, string& filename_extension
 	dat >> program_name >> version;
 	if (program_name != "test_pro" || !dat.good())
 	{
-		cerr << "default.dat"<<error_message<<'\n';
+		cerr << "default.dat" << error_message << '\n';
 		return;
 	}
 	try
 	{
-		dat.ignore(numeric_limits<streamsize>::max(),'\n');//next line
-		if(!getline(dat, exe_path))							throw "exe path";
-		if(!getline(dat,file_dir))								throw "file directory";
-		if(!getline(dat, filename_extension))		throw "filename extension";
-		if(!(dat >> start_num >> end_num))		throw "range";
+		dat.ignore(numeric_limits<streamsize>::max(), '\n');//next line
+		if (!getline(dat, exe_path))							throw "exe path";
+		if (!getline(dat, file_dir))								throw "file directory";
+		if (!getline(dat, filename_extension))		throw "filename extension";
+		if (!(dat >> start_num >> end_num))		throw "range";
 	}
 	catch (const char *err)
 	{
-		cerr << err <<error_message<< '\n';
+		cerr << err << error_message << '\n';
 	}
-	
+
 	if (version >= 2.0)
 	{
-		dat.ignore(numeric_limits<streamsize>::max(),'\n');//next line
+		dat.ignore(numeric_limits<streamsize>::max(), '\n');//next line
 		getline(dat, diff_option);
 	}
 	dat.close();
@@ -76,8 +77,8 @@ void read_default(string& exe_path, string& file_dir, string& filename_extension
 }
 void read_options(string& exe_path, string& file_dir, string& filename_extension, int& start_num, int& end_num, string &single_file, string& diff_option)
 {
-	string option_key,str;
-	while (cin>>option_key)
+	string option_key, str;
+	while (cin >> option_key)
 	{
 		if (option_key == "run" || option_key == "r")
 			break;
@@ -109,20 +110,20 @@ void read_options(string& exe_path, string& file_dir, string& filename_extension
 			}
 		}
 		else if (option_key == "-s")
-			single_file+=' '+ str;
+			single_file += ' ' + str;
 		else if (option_key == "-c")
-				diff_option = str;
+			diff_option = str;
 		else
 			cerr << "unknown option\n";
 	}
 #ifndef _WIN32
 #include <cstdlib>
-	if (exe_path[0]=='~')
-	{	
+	if (exe_path[0] == '~')
+	{
 		exe_path.erase(exe_path.begin());
 		exe_path.insert(0, getenv("HOME"));
 	}
-	if(file_dir[0]=='~')
+	if (file_dir[0] == '~')
 	{
 		file_dir.erase(file_dir.begin());
 		file_dir.insert(0, getenv("HOME"));
@@ -130,39 +131,39 @@ void read_options(string& exe_path, string& file_dir, string& filename_extension
 #endif //_WIN32
 }
 
-bool write_bat(const string& exe_path, const string& file_dir, const string& filename_extension, const int start_num, const int end_num,const string& single_files )
-{	
+bool write_bat(const string& exe_path, const string& file_dir, const string& filename_extension, const int start_num, const int end_num, const string& single_files)
+{
 #ifdef _WIN32
-	const string cd = "cd /d \""+file_dir+'\"';
+	const string cd = "cd /d \"" + file_dir + '\"';
 #else
-	const string cd=(file_dir.find(' ')==string::npos)?("cd "+file_dir):("cd \""+file_dir+'\"');
+	const string cd = (file_dir.find(' ') == string::npos) ? ("cd " + file_dir) : ("cd \"" + file_dir + '\"');
 #endif // _WIN32
 #ifdef DEBUG
-	cout<<"cd#"<<cd<<"#\n";
+	cout << "cd#" << cd << "#\n";
 	SYSTEMPAUSE;
 #endif //DEBUG
-	if(system(cd))//failed to cd
+	if (system(cd))//failed to cd
 		return false;
 
-	string script_path=file_dir + os_delim+ shell_script;
+	string script_path = file_dir + os_delim + shell_script;
 	ofstream script(script_path, ios::out | ios::trunc);
 	if (script.fail())
 	{
-		cerr << "can not open " << script_path <<'\n' ;
+		cerr << "can not open " << script_path << '\n';
 		return false;
 	}
 
 #ifndef _WIN32
-	script<<"set -x\n";//echo shell commands
+	script << "set -x\n";//echo shell commands
 #endif //_WIN32
-	script << cd <<'\n';
+	script << cd << '\n';
 #ifdef _WIN32
 	const string to_do = ") do \"" + exe_path + "\" <%%A" + filename_extension + " >%%A_out.txt\n";
-	script << "for /L %%A in (" << start_num << ",1," << end_num<<to_do;
-	script <<"for %%A in ("<< single_files<< to_do;
+	script << "for /L %%A in (" << start_num << ",1," << end_num << to_do;
+	script << "for %%A in (" << single_files << to_do;
 #else
 	const string to_do = "; do \"" + exe_path + "\" <\"$a\"" + filename_extension + " >\"$a\"_out.txt; done\n";
-	script << "for a in $(seq " << start_num << ' ' << end_num <<')'<< to_do;
+	script << "for a in $(seq " << start_num << ' ' << end_num << ')' << to_do;
 	script << "for a in " << single_files << to_do;
 #endif // _WIN32
 	script.close();
@@ -175,7 +176,7 @@ void compare_answer(ifstream& your_ans, ifstream& correct_ans, const string& fil
 	while (!your_ans.eof() || !correct_ans.eof())
 	{
 		string ans, right_ans;
-		getline(your_ans,ans);
+		getline(your_ans, ans);
 		getline(correct_ans, right_ans);
 
 		if (ans != right_ans)
@@ -184,8 +185,8 @@ void compare_answer(ifstream& your_ans, ifstream& correct_ans, const string& fil
 			string str;
 			your_ans.seekg(0);
 			correct_ans.seekg(0);
-			
-			cout <<"\nwrong answer:" << filepath.substr(filepath.find_last_of(os_delim)+1) << '\n';
+
+			cout << "\nwrong answer:" << filepath.substr(filepath.find_last_of(os_delim) + 1) << '\n';
 			cout << "your answers:\n";
 			while (getline(your_ans, str))
 				cout << str << '\n';
@@ -200,23 +201,23 @@ void compare_answer(ifstream& your_ans, ifstream& correct_ans, const string& fil
 	if (accepted)
 		cout << filepath.substr(filepath.find_last_of(os_delim) + 1) << "\taccepted\n";
 }
-void diff_answer(ifstream& your_ans, ifstream& correct_ans,const string &diff_option, const string& my_ans_path, const string& correct_ans_path)
+void diff_answer(ifstream& your_ans, ifstream& correct_ans, const string &diff_option, const string& my_ans_path, const string& correct_ans_path)
 {
 	//enclose the path in quotes
-	string test = diff_path  + '\"' + correct_ans_path + "\" \"" + my_ans_path + "\" "+ diff_option;
-//if it's windows, enclose the whole command in quotes
+	string test = diff_path + '\"' + correct_ans_path + "\" \"" + my_ans_path + "\" " + diff_option;
+	//if it's windows, enclose the whole command in quotes
 #ifdef _WIN32
-	test.insert(test.begin(),'\"');
-	test+='\"';
+	test.insert(test.begin(), '\"');
+	test += '\"';
 #endif //_WIN32
 #ifdef DEBUG
 	cout << "test#" << test << "#\n";
 #endif // DEBUG
 
 	if (system(test))
-		cout << '\n' << my_ans_path.substr(my_ans_path.find_last_of(os_delim)+1)<< "\twrong answer\n\n";
+		cout << '\n' << my_ans_path.substr(my_ans_path.find_last_of(os_delim) + 1) << "\twrong answer\n\n";
 	else
-		cout << my_ans_path.substr(my_ans_path.find_last_of(os_delim)+1) << "\taccepted\n";
+		cout << my_ans_path.substr(my_ans_path.find_last_of(os_delim) + 1) << "\taccepted\n";
 }
 void init_check_ans(const string &file_dir, const string &filename, const string &diff_option)
 {
@@ -243,7 +244,7 @@ void init_check_ans(const string &file_dir, const string &filename, const string
 
 int main()
 {
-	string exe_path, file_dir, filename_extension, diff_option="n";
+	string exe_path, file_dir, filename_extension, diff_option = "n";
 	string str = "";
 	int start_num, end_num;
 
@@ -259,18 +260,18 @@ int main()
 	cout << "enter \"run\" or \"r\" to start test_pro\n";
 
 	string single_file;
-	read_options(exe_path, file_dir, filename_extension, start_num, end_num,single_file, diff_option);
+	read_options(exe_path, file_dir, filename_extension, start_num, end_num, single_file, diff_option);
 
 #ifdef DEBUG
 	cout << "exe_path#" << exe_path << "#\n";
-	cout << "file_dir#"<<file_dir<< "#\n";
-	cout << "filename_extension#" << filename_extension<< "#\n";
-	cout << "start_num#" << start_num<< "#end_num#"<<end_num<<"#\n";
-	cout << "single_file#" << single_file<< "#\n";
+	cout << "file_dir#" << file_dir << "#\n";
+	cout << "filename_extension#" << filename_extension << "#\n";
+	cout << "start_num#" << start_num << "#end_num#" << end_num << "#\n";
+	cout << "single_file#" << single_file << "#\n";
 	cout << "diff_option#" << diff_option << "#\n";
 	SYSTEMPAUSE;
 #endif
-	
+
 	//build script
 	if (!write_bat(exe_path, file_dir, filename_extension, start_num, end_num, single_file))
 	{
@@ -281,7 +282,7 @@ int main()
 
 	//use script
 #ifdef _WIN32
-	system('\"'+file_dir+ os_delim+ shell_script+'\"');
+	system('\"' + file_dir + os_delim + shell_script + '\"');
 #else
 	system("bash \"" + file_dir + os_delim + shell_script + '\"');
 #endif // _WIN32
